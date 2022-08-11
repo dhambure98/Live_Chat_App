5dca0517ab51e42e23c018bb5712e98b515a8b90
dhambure98 <akila789liyanage@gmail.com>
2022-08-11 23:37:46 +0530

    Add AppInitilizer and ClientUi/LoginForm\

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,2 +1,25 @@
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/lk/ijse/chat_project/view/LoginForm.fxml"))));
+        primaryStage.setMaximized(false);
+        primaryStage.setResizable(true);
+        primaryStage.show();
+       /* primaryStage.setMaximized(true);
+        primaryStage.setMaximized(false);*/
+
+    }
 }
