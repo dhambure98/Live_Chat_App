b231baca45bf73385ad0ff45fc5c10fc08a96c78
dhambure98 <akila789liyanage@gmail.com>
2022-08-12 18:17:13 +0530

    Update LogingFormController

--- src/lk/ijse/chat_project/controller/LoginFormController.java
+++ src/lk/ijse/chat_project/controller/LoginFormController.java
@@ -4,7 +4,12 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public AnchorPane root;
@@ -12,6 +17,27 @@ public class LoginFormController {
     public JFXTextField txtUserName;
     public JFXButton btnLog;
 
-    public void btnLogOnAction(ActionEvent actionEvent) {
+    public void btnLogOnAction(ActionEvent actionEvent) throws IOException {
+        String user = txtUserName.getText().trim();
+        String password = txtPw.getText().trim();
+        if (user.length() > 0 && password.length() > 0) {
+
+            if (user.equalsIgnoreCase("User")
+                    && password.equalsIgnoreCase("1234")) {
+
+                this.root.getChildren().clear();
+                this.root.getChildren()
+                        .add(FXMLLoader.
+                                load(this.getClass().getResource("ClientUi.fxml")));
+
+            } else {
+                new Alert(Alert.AlertType.WARNING, "Wrong Password Or User Name !",
+                        ButtonType.OK).show();
+            }
+        } else {
+            new Alert(Alert.AlertType.WARNING, "Empty !",
+                    ButtonType.OK).show();
+
+        }
     }
 }
