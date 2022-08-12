34c300da0c7b39b85543ae7cf22abe189d8a082d
dhambure98 <akila789liyanage@gmail.com>
2022-08-12 14:54:36 +0530

    Update Client_App Design  LoginForm fxml

--- src/lk/ijse/chat_project/controller/LoginFormController.java
+++ src/lk/ijse/chat_project/controller/LoginFormController.java
@@ -1,4 +1,17 @@
 package lk.ijse.chat_project.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane root;
+    public JFXPasswordField txtPw;
+    public JFXTextField txtUserName;
+    public JFXButton btnLog;
+
+    public void btnLogOnAction(ActionEvent actionEvent) {
+    }
 }
