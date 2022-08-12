bc74413d4539f575a08ee8d108e331113e6f7151
dhambure98 <akila789liyanage@gmail.com>
2022-08-12 17:01:14 +0530

    Update Client_App Design ClientUi fxml

--- src/lk/ijse/chat_project/controller/ClientUiController.java
+++ src/lk/ijse/chat_project/controller/ClientUiController.java
@@ -1,4 +1,21 @@
 package lk.ijse.chat_project.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class ClientUiController {
+    public TextArea txtMessageArea;
+    public TextField txtMessage;
+    public JFXTextField txtName;
+
+    public void sendMessage(MouseEvent mouseEvent) {
+    }
+
+    public void photoUpload(MouseEvent mouseEvent) {
+    }
+
+    public void emojiUpload(MouseEvent mouseEvent) {
+    }
 }
